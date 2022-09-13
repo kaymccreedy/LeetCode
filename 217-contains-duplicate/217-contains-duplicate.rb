76242d3bc5ee0ab78b78a69eb72f13f3763f849b
Kay McCreedy <107575694+kaymccreedy@users.noreply.github.com>
2022-09-13 14:04:33 -0400

    Time: 264 ms (9.54%), Space: 219.1 MB (21.38%) - LeetHub

---
+++ 217-contains-duplicate/217-contains-duplicate.rb
@@ -0,0 +1,10 @@
+# @param {Integer[]} nums
+# @return {Boolean}
+def contains_duplicate(nums)
+    hash = {}
+    nums.each do |x|
+        return true if hash[x] == x
+        hash[x] = x
+    end
+    false
+end
